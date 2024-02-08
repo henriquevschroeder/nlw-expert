19724c8239217e9a5f1fad30143f23c0ad65156e
Henrique Valiati Schroeder <henriquevschdev@gmail.com>
2024-02-08 00:23:50 -0300

    feat: create poll options along with the poll

--- nlw-expert-polls/src/http/routes/create-poll.ts
+++ nlw-expert-polls/src/http/routes/create-poll.ts
@@ -6,13 +6,21 @@ export async function createPoll(app: FastifyInstance) {
   app.post('/polls', async (request, reply) => {
     const bodySchema = z.object({
       title: z.string(),
+      options: z.array(z.string())
     })
   
-    const { title } = bodySchema.parse(request.body)
+    const { title, options } = bodySchema.parse(request.body)
   
     const poll = await prisma.poll.create({
       data: {
-        title
+        title,
+        options: {
+          createMany: {
+            data: options.map(option => {
+              return { title: option }
+            })
+          }
+        }
       }
     })
   
