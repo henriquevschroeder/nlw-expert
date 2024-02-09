52cbe67e9784d2f9cb03dda20aa10a78b3c38e56
Henrique Valiati Schroeder <henriquevschdev@gmail.com>
2024-02-08 22:34:14 -0300

    feat: add voting functionality

--- nlw-expert-polls/src/http/server.ts
+++ nlw-expert-polls/src/http/server.ts
@@ -1,11 +1,22 @@
 import fastify from "fastify"
+import cookie from "@fastify/cookie"
 import { createPoll } from "./routes/create-poll"
 import { getPoll } from "./routes/get-poll"
+import { voteOnPoll } from "./routes/vote-on-poll"
 
 const app = fastify()
 
+// Plugins
+app.register(cookie, {
+  secret: 'polls-app-nlw', // no need to use .env for this project
+  hook: 'onRequest',
+  parseOptions: {}
+})
+
+// Routes
 app.register(createPoll)
 app.register(getPoll)
+app.register(voteOnPoll)
 
 const port = 3333
 
