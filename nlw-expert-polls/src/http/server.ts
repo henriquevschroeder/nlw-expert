1e853ac31106b3dd8423e16a3c09132fc7a3e890
Henrique Valiati Schroeder <henriquevschdev@gmail.com>
2024-02-08 23:27:59 -0300

    feat: add websocket for real-time votes using pub-sub pattern

--- nlw-expert-polls/src/http/server.ts
+++ nlw-expert-polls/src/http/server.ts
@@ -1,8 +1,10 @@
 import fastify from "fastify"
 import cookie from "@fastify/cookie"
+import websocket from "@fastify/websocket"
 import { createPoll } from "./routes/create-poll"
 import { getPoll } from "./routes/get-poll"
 import { voteOnPoll } from "./routes/vote-on-poll"
+import { pollResults } from "./ws/poll-results"
 
 const app = fastify()
 
@@ -12,11 +14,13 @@ app.register(cookie, {
   hook: 'onRequest',
   parseOptions: {}
 })
+app.register(websocket)
 
 // Routes
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(pollResults)
 
 const port = 3333
 
