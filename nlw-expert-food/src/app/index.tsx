0023681ca8b47248bb77413584bd824aac172306
Henrique Valiati Schroeder <henriquevschdev@gmail.com>
2024-02-12 20:26:35 -0300

    feat: add inter google font

--- nlw-expert-food/src/app/index.tsx
+++ nlw-expert-food/src/app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native'
 export default function Home() {
   return (
     <View className="flex-1 pt-8">
-      <Text className="text-white text-2xl">Hello, World!</Text>
+      <Text className="text-white text-2xl font-heading">Hello, World!</Text>
     </View>
   )
 }
