5d4cf5439f49a465487ccef3b36617c15ae40fef
Henrique Valiati Schroeder <henriquevschdev@gmail.com>
2024-02-12 20:11:58 -0300

    feat: add nativewind to project

--- nlw-expert-food/src/app/index.tsx
+++ nlw-expert-food/src/app/index.tsx
@@ -2,8 +2,8 @@ import { View, Text } from 'react-native'
 
 export default function Home() {
   return (
-    <View>
-      <Text>Hello, World!</Text>
+    <View className="flex-1 pt-8">
+      <Text className="text-white text-2xl">Hello, World!</Text>
     </View>
   )
 }
