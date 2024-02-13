d58937897f4d4dd8fe56e71f3fb14aa137ba6355
Henrique Valiati Schroeder <henriquevschdev@gmail.com>
2024-02-12 21:43:49 -0300

    feat: add buttons for each category

--- nlw-expert-food/src/app/index.tsx
+++ nlw-expert-food/src/app/index.tsx
@@ -1,10 +1,44 @@
+import { useState } from 'react'
+import { View, FlatList } from 'react-native'
+import { CATEGORIES } from '@/utils/data/products'
 import { Header } from '@/components/header'
-import { View, Text } from 'react-native'
+import { CategoryButton } from '@/components/category-button'
 
 export default function Home() {
+
+  const [category, setCategory] = useState(CATEGORIES[0])
+
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory)
+  }
+
   return (
     <View className="flex-1 pt-8">
       <Header title="Place your order" cartItemsQuantity={5} />
+
+      {/* Categories */}
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton 
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelect(item)}
+          />
+        )}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
+
+      {/* <View className="flex-row gap-4">
+        <CategoryButton title="Something" isSelected />
+        <CategoryButton title="Something" />
+        <CategoryButton title="Something" />
+      </View> */}
+
     </View>
   )
 }
